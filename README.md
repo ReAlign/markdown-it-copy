a19c3bcf7a8ce7bf3bed4a36a1a30f75631bf2cf
realign <realign@yeah.net>
2019-12-19 17:11:36 +0800

    re:first-version publish

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # markdown-it-copy
-A vuepress-plugin that copy code and others.
+
+[![NPM version][npm-image]][npm-url]
+[![download][downloads-image]][github-url]
+[![NPM][nodei-image]][npm-url]
+
+> A vuepress-plugin that copy code and others.
+
+## Installation
+
+```bash
+npm install markdown-it-copy -S
+```
+
+## Usage
+
+### In-Plugin
+
+```js
+const options = {
+    btnText: '', // 'Copy' | button text
+    extraHtmlBeforeBtn: '', // '' | a html-fragment before <button>
+    extraHtmlAfterBtn: '', // '' | a html-fragment after <button>
+};
+
+const md = require('markdown-it')()
+            .use(require('markdown-it-copy'), [, options]);
+```
+
+### In-PageAfterMarkdownRendered
+
+It will call `window.MDIC.btnClick` when copy-button clicked, so need a window object in the page that after markdown rendered. as:
+
+```js
+window.MDIC = {
+    // btn is button-element
+    // copy-code in: btn.dataset.mdiccont
+    btnClick(btn) {
+        // Todo
+    },
+};
+```
+
+## ChangeLog
+
+* v0.1.0
+  * 2019-12-19
+
+[github-url]: https://github.com/ReAlign/markdown-it-copy
+[npm-url]: https://www.npmjs.com/package/markdown-it-copy
+[npm-image]: https://img.shields.io/npm/v/markdown-it-copy.svg
+[downloads-image]: https://img.shields.io/npm/dt/markdown-it-copy.svg
+[nodei-image]: https://nodei.co/npm/markdown-it-copy.png?downloads=true&downloadRank=true&stars=true
--- index.js
+++ index.js
@@ -1,3 +1,49 @@
-const copy = () => {};
+const LAST_TAG = '</pre>';
+/**
+ * @name  enhance
+ * @param {*} render    | origin-render-function
+ * @param {*} options   | options
+ * ------
+ * mdic: abbreviation of "markdown-it-copy"
+ */
+const enhance = (render, options = {}) => (...args) => {
+  /* args = [tokens, idx, options, env, slf] */
+  const {
+    btnText = 'Copy', // 'Copy' | button text
+    extraHtmlBeforeBtn = '', // '' | a html-fragment before <button>
+    extraHtmlAfterBtn = '', // '' | a html-fragment after <button>
+  } = options;
+  const [tokens = {}, idx = 0] = args;
+  // origin writed-code
+  const cont = tokens[idx].content || '';
+  const copyTag = `
+    <div class="m-mdic-wrapper">
+      ${extraHtmlBeforeBtn}
+      <button
+        class="u-mdic-btn"
+        data-mdiccont="${cont}"
+        onclick="window.MDIC.btnClick(this)">
+        ${btnText}
+      </button>
+      ${extraHtmlAfterBtn}
+    </div>
+  `;
 
-module.exports = copy;
+  const originResult = render.apply(this, args);
+  const newResult = originResult.replace(LAST_TAG, `${copyTag}${LAST_TAG}`);
+
+  return newResult;
+};
+
+/**
+ * @name  copy
+ * @param md        md
+ * @param options   options
+ */
+module.exports = (md = {}, options = {}) => {
+  const codeBlockRender = md.renderer.rules.code_block;
+  const fenceRender = md.renderer.rules.code_block;
+
+  md.renderer.rules.code_block = enhance(codeBlockRender, options);
+  md.renderer.rules.fence = enhance(fenceRender, options);
+};
